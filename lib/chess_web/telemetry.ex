59672876bb19aba1a523a6ed2e96a8c491143743
Zack Kayser <zkayser@cars.com>
2021-11-15 08:42:54 -0500

    Initialize repo to align with credo rules

--- lib/chess_web/telemetry.ex
+++ lib/chess_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule ChessWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -17,6 +17,9 @@ defmodule ChessWeb.ConnCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+  alias Phoenix.ConnTest
+
   using do
     quote do
       # Import conveniences for testing with connections
@@ -32,8 +35,8 @@ defmodule ChessWeb.ConnCase do
   end
 
   setup tags do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Chess.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
-    {:ok, conn: Phoenix.ConnTest.build_conn()}
+    pid = Sandbox.start_owner!(Chess.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
+    {:ok, conn: ConnTest.build_conn()}
   end
 end
