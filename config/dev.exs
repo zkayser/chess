bf1da51c989fed375d33178ba307a838c84c0854
Zack Kayser <zkayser@cars.com>
2021-11-15 08:51:13 -0500

    Add pre and post-conditons with Oath for Square.init/1

--- config/dev.exs
+++ config/dev.exs
@@ -66,6 +66,9 @@ config :chess, ChessWeb.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+# Oath
+config :oath, enable_contracts: true
+
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
