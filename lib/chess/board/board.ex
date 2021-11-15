2dcf548c96a2eb0b0ca42eb9a38674f07bd0009b
Zack Kayser <zkayser@cars.com>
2021-11-15 08:58:29 -0500

    Add assertion that Board.layout/0 creates a Square for each element

--- lib/chess/board/board.ex
+++ lib/chess/board/board.ex
@@ -2,11 +2,13 @@ defmodule Chess.Board do
   @moduledoc """
   Representation of a chess board.
   """
+  alias Chess.Board.Square
 
-  @type t :: :array.array(term())
+  @type t :: :array.array(Square.t())
 
   @spec layout() :: t()
   def layout do
-    :array.new(size: 64, fixed: true, default: :empty)
+    board = :array.new(size: 64, fixed: true, default: :empty)
+    :array.map(fn index, _ -> Square.init(index) end, board)
   end
 end
--- test/chess/board/board_test.exs
+++ test/chess/board/board_test.exs
@@ -2,10 +2,13 @@ defmodule Chess.BoardTest do
   use ExUnit.Case
 
   alias Chess.Board
+  alias Chess.Board.Square
 
   describe "layout/0" do
     test "creates a board of 64 squares" do
-      assert :array.size(Board.layout()) == 64
+      board = Board.layout()
+      assert :array.size(board) == 64
+      :array.map(fn _, square -> match?(%Square{}, square) end, board)
     end
   end
 end
