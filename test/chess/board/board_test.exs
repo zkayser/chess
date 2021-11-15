d433c3b6d796e5cb4f44144d44ee534228d90b85
Zack Kayser <zkayser@cars.com>
2021-11-15 08:04:42 -0500

    Use array:size/1 for length assertion

--- test/chess/board/board_test.exs
+++ test/chess/board/board_test.exs
@@ -5,7 +5,7 @@ defmodule Chess.BoardTest do
 
   describe "layout/0" do
     test "creates a board of 64 squares" do
-      assert length(Board.layout()) == 64
+      assert :array.size(Board.layout()) == 64
     end
   end
 end
