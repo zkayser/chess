db24e17d24666e5fab22d50cd10114b122364102
Zack Kayser <zkayser@cars.com>
2021-11-15 08:30:13 -0500

    Add failing property test for Square.init/1 with odd indices

--- test/chess/board/square_test.exs
+++ test/chess/board/square_test.exs
@@ -6,9 +6,15 @@ defmodule Chess.Board.SquareTest do
   require Integer
 
   describe "init/1" do
-    property "returns a white square when given parameter is even" do
+    property "returns a white square when given index is even" do
       check all index <- filter(integer(0..64), &Integer.is_even/1) do
         assert %Square{color: :white} = Square.init(index)
+      end
+    end
+
+    property "returns a black square when given index is odd" do
+      check all index <- filter(integer(0..64), &Integer.is_odd/1) do
+        assert %Square{color: :black} = Square.init(index)
       end
     end
   end
