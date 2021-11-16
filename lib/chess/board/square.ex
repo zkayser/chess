39ea0adcfabc6407e78c7a00bc19cecb306474f9
Zack Kayser <zkayser@cars.com>
2021-11-16 07:27:56 -0500

    Define Piece.t/0 type and implement placement for pawns

--- lib/chess/board/square.ex
+++ lib/chess/board/square.ex
@@ -6,20 +6,35 @@ defmodule Chess.Board.Square do
 
   import Integer, only: [is_even: 1]
 
+  alias Chess.Piece
+
   @type t() :: %__MODULE__{
-    color: color()
+    color: color(),
+    piece: Piece.t() | :empty
   }
   @opaque color :: :white | :black
+  @typep index :: non_neg_integer()
 
-  defstruct [:color]
+  defstruct [
+    color: nil,
+    piece: :empty
+  ]
 
   @decorate pre("Index is a non-negative integer between 0 and 64", fn index -> index in 0..64 end)
   @spec init(non_neg_integer()) :: t()
   def init(index) do
-    %__MODULE__{color: color_for(index)}
+    %__MODULE__{
+      color: color_for(index),
+      piece: piece_for(index)
+    }
   end
 
-  @spec color_for(index :: non_neg_integer()) :: color()
+  @spec color_for(index()) :: color()
   defp color_for(index) when is_even(index), do: :white
   defp color_for(_index), do: :black
+
+  @pawn_indices Enum.concat(8..15, 48..55)
+  @spec piece_for(index()) :: Piece.t() | :empty
+  defp piece_for(index) when index in @pawn_indices, do: :pawn
+  defp piece_for(_), do: :empty
 end
