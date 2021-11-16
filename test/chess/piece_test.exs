896bebf7ce089ecefb934dd5077d95888825308a
Zack Kayser <zkayser@cars.com>
2021-11-16 12:10:41 -0500

    Update piece test to reflect new representation of individual pieces

--- test/chess/piece_test.exs
+++ test/chess/piece_test.exs
@@ -3,6 +3,7 @@ defmodule Chess.PieceTest do
   doctest Chess.Piece
 
   alias Chess.Piece
+  alias Chess.Pieces.{Bishop, King, Knight, Pawn, Queen, Rook}
 
   describe "for_starting_position/1" do
     @pawn_indices Enum.concat(8..15, 48..55)
@@ -15,37 +16,37 @@ defmodule Chess.PieceTest do
 
     for pawn_index <- @pawn_indices do
       test "places a pawn at #{pawn_index} index" do
-        assert %Piece{type: :pawn} = Piece.for_starting_position(unquote(pawn_index))
+        assert %Piece{type: Pawn} = Piece.for_starting_position(unquote(pawn_index))
       end
     end
 
     for rook_index <- @rook_indices do
       test "places a rook at #{rook_index} index" do
-        assert %Piece{type: :rook} = Piece.for_starting_position(unquote(rook_index))
+        assert %Piece{type: Rook} = Piece.for_starting_position(unquote(rook_index))
       end
     end
 
     for knight_index <- @knight_indices do
       test "places a knight at #{knight_index} index" do
-        assert %Piece{type: :knight} = Piece.for_starting_position(unquote(knight_index))
+        assert %Piece{type: Knight} = Piece.for_starting_position(unquote(knight_index))
       end
     end
 
     for bishop_index <- @bishop_indices do
       test "places a bishop at #{bishop_index} index" do
-        assert %Piece{type: :bishop} = Piece.for_starting_position(unquote(bishop_index))
+        assert %Piece{type: Bishop} = Piece.for_starting_position(unquote(bishop_index))
       end
     end
 
     for queen_index <- @queen_indices do
       test "places a queen at #{queen_index} index" do
-        assert %Piece{type: :queen} = Piece.for_starting_position(unquote(queen_index))
+        assert %Piece{type: Queen} = Piece.for_starting_position(unquote(queen_index))
       end
     end
 
     for king_index <- @king_indices do
       test "places a king at #{king_index} index" do
-        assert %Piece{type: :king} = Piece.for_starting_position(unquote(king_index))
+        assert %Piece{type: King} = Piece.for_starting_position(unquote(king_index))
       end
     end
 
