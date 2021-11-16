83efd2266dfc93f8d5c4b6454c776e5871b0e59e
Zack Kayser <zkayser@cars.com>
2021-11-16 08:02:18 -0500

    Implement Chess.Piece.for_starting_position/1

--- lib/chess/piece.ex
+++ lib/chess/piece.ex
@@ -10,7 +10,46 @@ defmodule Chess.Piece do
     color: color()
   } | nil
   @type type() :: :pawn | :rook | :knight | :bishop | :queen | :king
-  @opaque color :: :white | :black
+  @opaque color() :: :white | :black
+  @typep index :: non_neg_integer()
 
   defstruct [:type, :color]
+
+  # Starting Positions
+  @pawn_indices Enum.concat(8..15, 48..55)
+  @rook_indices [0, 7, 56, 63]
+  @knight_indices [1, 6, 57, 62]
+  @bishop_indices [2, 5, 58, 61]
+  @queen_indices [3, 59]
+  @king_indices [4, 60]
+  @empty_indices 16..47
+
+
+  @doc """
+  Given a starting index, returns the piece that is placed
+  at that index.
+
+  iex> Chess.Piece.for_starting_position(0)
+  %Chess.Piece{type: :rook, color: :black}
+  """
+  @spec for_starting_position(index()) :: t()
+  def for_starting_position(index) when index in @empty_indices, do: nil
+  def for_starting_position(index) do
+    %__MODULE__{
+      type: type_at_starting_position(index),
+      color: if(index in 0..15, do: :black, else: :white)
+    }
+  end
+
+  @spec type_at_starting_position(index()) :: type()
+  defp type_at_starting_position(index) do
+    case index do
+      i when i in @pawn_indices -> :pawn
+      i when i in @rook_indices -> :rook
+      i when i in @knight_indices -> :knight
+      i when i in @bishop_indices -> :bishop
+      i when i in @queen_indices -> :queen
+      i when i in @king_indices -> :king
+    end
+  end
 end
