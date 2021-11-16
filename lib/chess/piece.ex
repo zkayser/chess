f8540f3dc02b2bce038ca3ae37781a781624f262
Zack Kayser <zkayser@cars.com>
2021-11-16 07:27:56 -0500

    Define Piece.t/0 type and implement placement for pawns

---
+++ lib/chess/piece.ex
@@ -0,0 +1,8 @@
+defmodule Chess.Piece do
+  @moduledoc """
+  Representation of a chess piece and associated
+  functions.
+  """
+
+  @type t() :: :pawn | :rook | :knight | :bishop | :queen | :king
+end
