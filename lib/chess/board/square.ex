d5196d0b14b01cc962572d046d48bf256646c48b
Zack Kayser <zkayser@cars.com>
2021-11-16 08:42:26 -0500

    Assert board layout places pieces in the first two rows on both sides of the board and keeps the rest of the rows empty.

--- lib/chess/board/square.ex
+++ lib/chess/board/square.ex
@@ -10,7 +10,7 @@ defmodule Chess.Board.Square do
 
   @type t() :: %__MODULE__{
     color: color(),
-    piece: Piece.t() | :empty
+    piece: Piece.t()
   }
   @opaque color :: :white | :black
   @typep index :: non_neg_integer()
@@ -25,26 +25,11 @@ defmodule Chess.Board.Square do
   def init(index) do
     %__MODULE__{
       color: color_for(index),
-      piece: piece_for(index)
+      piece: Piece.for_starting_position(index)
     }
   end
 
   @spec color_for(index()) :: color()
   defp color_for(index) when is_even(index), do: :white
   defp color_for(_index), do: :black
-
-  @pawn_indices Enum.concat(8..15, 48..55)
-  @rook_indices [0, 7, 56, 63]
-  @knight_indices [1, 6, 57, 62]
-  @bishop_indices [2, 5, 58, 61]
-  @queen_indices [3, 59]
-  @king_indices [4, 60]
-  @spec piece_for(index()) :: Piece.t() | :empty
-  defp piece_for(index) when index in @pawn_indices, do: :pawn
-  defp piece_for(index) when index in @rook_indices, do: :rook
-  defp piece_for(index) when index in @knight_indices, do: :knight
-  defp piece_for(index) when index in @bishop_indices, do: :bishop
-  defp piece_for(index) when index in @queen_indices, do: :queen
-  defp piece_for(index) when index in @king_indices, do: :king
-  defp piece_for(_), do: :empty
 end
