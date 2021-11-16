44ca7815a96e316ddc921a4c6ae45ad419d40c14
Zack Kayser <zkayser@cars.com>
2021-11-16 07:20:09 -0500

    Setup failing tests for placing pawns in squares

--- test/chess/board/square_test.exs
+++ test/chess/board/square_test.exs
@@ -6,6 +6,8 @@ defmodule Chess.Board.SquareTest do
   require Integer
 
   describe "init/1" do
+    @pawn_indices Enum.concat(8..15, 48..55)
+
     property "returns a white square when given index is even" do
       check all index <- filter(integer(0..64), &Integer.is_even/1) do
         assert %Square{color: :white} = Square.init(index)
@@ -15,6 +17,12 @@ defmodule Chess.Board.SquareTest do
     property "returns a black square when given index is odd" do
       check all index <- filter(integer(0..64), &Integer.is_odd/1) do
         assert %Square{color: :black} = Square.init(index)
+      end
+    end
+
+    for pawn_index <- @pawn_indices do
+      test "places a pawn at #{pawn_index} index" do
+        assert %Square{piece: :pawn} = Square.init(unquote(pawn_index))
       end
     end
   end
