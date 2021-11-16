b6ee217cca7a4b961f93f435c7bb48be9dff09ae
Zack Kayser <zkayser@cars.com>
2021-11-16 08:42:26 -0500

    Assert board layout places pieces in the first two rows on both sides of the board and keeps the rest of the rows empty.

--- test/chess/board/board_test.exs
+++ test/chess/board/board_test.exs
@@ -3,12 +3,27 @@ defmodule Chess.BoardTest do
 
   alias Chess.Board
   alias Chess.Board.Square
+  alias Chess.Piece
 
   describe "layout/0" do
     test "creates a board of 64 squares" do
       board = Board.layout()
       assert :array.size(board) == 64
       :array.map(fn _, square -> match?(%Square{}, square) end, board)
+    end
+
+    test "places chess pieces on squares 0 through 15 and 48 through 63" do
+      board = Board.layout()
+      for non_empty_square <- Enum.concat(0..15, 48..63) do
+        assert %Square{piece: %Piece{}} = :array.get(non_empty_square, board)
+      end
+    end
+
+    test "square 16 through 47 are empty" do
+      board = Board.layout()
+      for empty_square <- 16..47 do
+        assert %Square{piece: nil} = :array.get(empty_square, board)
+      end
     end
   end
 end
