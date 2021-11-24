2fa55f90f92852cd491236eb796fb4bb45851707
Zack Kayser <zkayser@cars.com>
2021-11-24 16:36:14 -0500

    Add test case for Pawn.potential_moves/3 given pawns on their first move

--- test/chess/pieces/pawn_test.exs
+++ test/chess/pieces/pawn_test.exs
@@ -21,6 +21,25 @@ defmodule Chess.Pieces.PawnTest do
     end
   end
 
+  describe "potential_moves/3 with no prior moves taken at beginning of game" do
+    for starting_index <- Enum.concat(8..15, 48..55) do
+      test "allows the pawn starting at #{starting_index} to move up one or two rows" do
+        {color, orientation} =
+          case unquote(starting_index) <= 15 do
+            true -> {:black, 1}
+            false -> {:white, -1}
+          end
+
+        pawn = %Piece{type: Pawn, color: color, moves: []}
+
+        assert MapSet.new([
+                 unquote(starting_index) + 8 * orientation,
+                 unquote(starting_index) + 16 * orientation
+               ]) == Pawn.potential_moves(pawn, unquote(starting_index), Board.layout())
+      end
+    end
+  end
+
   def piece_generator do
     gen all(
           type <- constant(Pawn),
