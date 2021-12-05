47f0f05e329cbe1d06bcab8939632f3f603db5b3
Zack Kayser <zkayser@cars.com>
2021-12-05 10:54:18 -0500

    Implement Access callbacks for Board.t/0

--- test/chess/board/board_test.exs
+++ test/chess/board/board_test.exs
@@ -8,15 +8,15 @@ defmodule Chess.BoardTest do
   describe "layout/0" do
     test "creates a board of 64 squares" do
       board = Board.layout()
-      assert :array.size(board) == 64
-      :array.map(fn _, square -> match?(%Square{}, square) end, board)
+      assert :array.size(board.board) == 64
+      :array.map(fn _, square -> match?(%Square{}, square) end, board.board)
     end
 
     test "places chess pieces on squares 0 through 15 and 48 through 63" do
       board = Board.layout()
 
       for non_empty_square <- Enum.concat(0..15, 48..63) do
-        assert %Square{piece: %Piece{}} = :array.get(non_empty_square, board)
+        assert %Square{piece: %Piece{}} = :array.get(non_empty_square, board.board)
       end
     end
 
@@ -24,7 +24,7 @@ defmodule Chess.BoardTest do
       board = Board.layout()
 
       for empty_square <- 16..47 do
-        assert %Square{piece: nil} = :array.get(empty_square, board)
+        assert %Square{piece: nil} = :array.get(empty_square, board.board)
       end
     end
   end
@@ -60,6 +60,9 @@ defmodule Chess.BoardTest do
                color: :white,
                piece: %Piece{type: Chess.Pieces.Rook, color: :black, moves: []}
              } == Board.square_at(Board.layout(), 0)
+    end
+  end
+
   describe "Access callbacks" do
     setup do
       {:ok, board: Board.layout()}
@@ -72,7 +75,41 @@ defmodule Chess.BoardTest do
     end
 
     test "fetch/2 returns an error when the given index is invalid", %{board: board} do
-      assert :error = board[-1]
+      assert is_nil(board[-1])
+    end
+
+    test "get_and_update/3 allows squares to be updated", %{board: board} do
+      new_square = %Square{
+        color: :white,
+        piece: %Piece{type: Chess.Pieces.King, color: :white, moves: []}
+      }
+
+      current_square = %Square{
+        color: :white,
+        piece: %Piece{type: Chess.Pieces.Rook, color: :black, moves: []}
+      }
+
+      assert {^current_square, new_board} =
+               Board.get_and_update(board, 0, fn current -> {current, new_square} end)
+
+      assert new_square == new_board[0]
+    end
+
+    test "pop/2 returns the square at the given index but is a no-op on the board", %{
+      board: board
+    } do
+      square = %Square{
+        color: :white,
+        piece: %Piece{type: Chess.Pieces.Rook, color: :black, moves: []}
+      }
+
+      assert {^square, ^board} = Board.pop(board, 0)
+    end
+
+    test "pop/2 returns nil and the unmodified board if an invalid index is given", %{
+      board: board
+    } do
+      assert {nil, ^board} = Board.pop(board, -1)
     end
   end
 end
