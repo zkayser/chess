f5f07a63c48bfa887b1320344a8483f105423b47
Zack Kayser <zkayser@cars.com>
2021-12-05 10:54:18 -0500

    Implement Access callbacks for Board.t/0

--- lib/chess/board/board.ex
+++ lib/chess/board/board.ex
@@ -2,17 +2,24 @@ defmodule Chess.Board do
   @moduledoc """
   Representation of a chess board.
   """
+  @behaviour Access
+
   alias Chess.Board.Square
 
-  @opaque t :: :array.array(Square.t())
+  @type t :: %__MODULE__{
+          board: :array.array(Square.t())
+        }
+  @opaque board :: :array.array(Square.t())
   @type index :: non_neg_integer()
+
+  defstruct [:board]
 
   @bounds 0..63
 
   @spec layout() :: t()
   def layout do
     board = :array.new(size: 64, fixed: true, default: :empty)
-    :array.map(fn index, _ -> Square.init(index) end, board)
+    %__MODULE__{board: :array.map(fn index, _ -> Square.init(index) end, board)}
   end
 
   @spec bounds() :: Range.t(0, 63)
@@ -22,5 +29,37 @@ defmodule Chess.Board do
   def in_bounds?(index), do: index in @bounds
 
   @spec square_at(t(), index()) :: Square.t()
-  def square_at(board, index), do: :array.get(index, board)
+  def square_at(%__MODULE__{board: board}, index), do: :array.get(index, board)
+
+  @impl Access
+  @spec fetch(t(), index()) :: {:ok, Square.t()} | :error
+  def fetch(board, index) when index in @bounds do
+    {:ok, square_at(board, index)}
+  end
+
+  def fetch(_board, _index), do: :error
+
+  @impl Access
+  @spec get_and_update(t(), index(), (Square.t() -> {Square.t(), Square.t()})) ::
+          {Square.t(), t()}
+  def get_and_update(board, index, function) do
+    {current_square, new_square} = function.(board[index])
+
+    new_board =
+      :array.set(
+        index,
+        new_square,
+        board.board
+      )
+
+    {current_square, %__MODULE__{board: new_board}}
+  end
+
+  @impl Access
+  @spec pop(t(), index()) :: {Square.t(), t()}
+  def pop(board, index) when index in @bounds do
+    {square_at(board, index), board}
+  end
+
+  def pop(board, _index), do: {nil, board}
 end
