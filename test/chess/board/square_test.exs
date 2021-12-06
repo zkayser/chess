4a041b61dc8069e1309cbcbdd9c1b0bab50a4cf3
Zack Kayser <zkayser@cars.com>
2021-12-06 08:28:54 -0500

    Add test cases for new Square.occupiable?/2 function

--- test/chess/board/square_test.exs
+++ test/chess/board/square_test.exs
@@ -20,6 +20,23 @@ defmodule Chess.Board.SquareTest do
     end
   end
 
+  describe "occupiable/2" do
+    test "returns false if the square given is occupied by a piece of the same color" do
+      square = %Square{piece: %Piece{color: :white}}
+      refute Square.occupiable?(square, %Piece{color: :white})
+    end
+
+    test "returns true if the square given is occupied by a piece of the opposite color" do
+      square = %Square{piece: %Piece{color: :black}}
+      assert Square.occupiable?(square, %Piece{color: :white})
+    end
+
+    test "returns true if the square given is empty" do
+      square = %Square{piece: nil}
+      assert Square.occupiable?(square, %Piece{color: :white})
+    end
+  end
+
   describe "occupied?/1" do
     test "returns false if the square given is not occupied by a chess piece" do
       refute Square.occupied?(%Square{piece: nil})
