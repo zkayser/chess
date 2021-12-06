099f06012d04440e5bfb4ae0932901db1c28ebf5
Zack Kayser <zkayser@cars.com>
2021-12-06 10:31:35 -0500

    Update Pawn potential moves function to capture all legal pawn movements

--- lib/chess/pieces/pawn.ex
+++ lib/chess/pieces/pawn.ex
@@ -13,7 +13,8 @@ defmodule Chess.Pieces.Pawn do
     moves
     |> list_of_potential_moves(starting_position, color)
     |> Stream.filter(&Board.in_bounds?/1)
-    |> Stream.reject(&Square.occupied?(board[&1]))
+    |> Stream.reject(&invalid_capture?(&1, starting_position, color, board))
+    |> Stream.reject(&occupied_non_capture?(&1, starting_position, board))
     |> MapSet.new()
   end
 
@@ -24,15 +25,54 @@ defmodule Chess.Pieces.Pawn do
       [] ->
         [
           starting_position + 8 * direction_for(color),
+          starting_position + 1 + 8 * direction_for(color),
+          starting_position - 1 + 8 * direction_for(color),
           starting_position + 16 * direction_for(color)
         ]
 
       _ ->
-        [starting_position + 8 * direction_for(color)]
+        [
+          starting_position + 8 * direction_for(color),
+          starting_position + 1 + 8 * direction_for(color),
+          starting_position - 1 + 8 * direction_for(color)
+        ]
     end
   end
 
   @spec direction_for(Piece.color()) :: -1 | 1
   defp direction_for(:white), do: -1
   defp direction_for(_), do: 1
+
+  @spec invalid_capture?(Square.index(), Square.index(), Piece.color(), Board.t()) :: boolean()
+  defp invalid_capture?(target, starting_point, color, board) do
+    case rem(abs(target - starting_point), 8) do
+      0 ->
+        false
+
+      _ ->
+        should_drop_diagonal?(target, color, board)
+    end
+  end
+
+  @spec should_drop_diagonal?(Square.index(), Piece.color(), Board.t()) :: boolean()
+  defp should_drop_diagonal?(target, color, board) do
+    case board[target] do
+      %Square{piece: %Piece{color: target_color}} when target_color != color ->
+        false
+
+      _ ->
+        true
+    end
+  end
+
+  @spec occupied_non_capture?(Square.index(), Square.index(), Board.t()) :: boolean()
+  defp occupied_non_capture?(target, starting_position, board) do
+    case rem(abs(target - starting_position), 8) do
+      0 ->
+        Square.occupied?(board[target])
+
+      _ ->
+        false
+    end
+  end
 end
