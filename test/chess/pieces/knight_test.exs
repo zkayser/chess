f4a7a81b6b7c8e66d3ced42c533b60571315a009
Zack Kayser <zkayser@cars.com>
2021-12-07 10:32:48 -0500

    Add simple test for Knight.potential_moves/3 with an empty board and knight starting in middle of board

---
+++ test/chess/pieces/knight_test.exs
@@ -0,0 +1,23 @@
+defmodule Chess.Pieces.KnightTest do
+  use ExUnit.Case
+
+  alias Chess.Board
+  alias Chess.Board.Square
+  alias Chess.Piece
+  alias Chess.Pieces.Knight
+  alias Chess.Test.BoardHelpers
+
+  describe "potential_moves/3 with empty board," do
+    test "allows knights in the middle of the board 4 potential moves" do
+      board = BoardHelpers.empty_board()
+      starting_index = 35
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([18, 20, 25, 29, 41, 45, 50, 52]) ==
+               Knight.potential_moves(piece, starting_index, board)
+    end
+  end
+end
