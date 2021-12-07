781a35d34ba2f3c2ccb87dec4e872638d19bc645
Zack Kayser <zkayser@cars.com>
2021-12-07 10:33:10 -0500

    WIP: Knight.potential_moves/3 implementation

--- lib/chess/pieces/knight.ex
+++ lib/chess/pieces/knight.ex
@@ -2,4 +2,32 @@ defmodule Chess.Pieces.Knight do
   @moduledoc """
   Represents a Knight piece.
   """
+  @behaviour Chess.Piece
+
+  alias Chess.Piece
+  alias Chess.Board
+  alias Chess.Board.Square
+
+  @impl Piece
+  def potential_moves(_knight, starting_position, board) do
+    starting_position
+    |> list_of_potential_moves()
+    |> Stream.filter(&Board.in_bounds?/1)
+    |> Stream.reject(&Square.occupied?(board[&1]))
+    |> MapSet.new()
+  end
+
+  @spec list_of_potential_moves(Board.index()) :: list(Board.index())
+  defp list_of_potential_moves(starting_index) do
+    [
+      starting_index + 8 * 2 + 1,
+      starting_index + 8 * 2 - 1,
+      starting_index - 8 * 2 + 1,
+      starting_index - 8 * 2 - 1,
+      starting_index + 2 + 8,
+      starting_index + 2 - 8,
+      starting_index - 2 + 8,
+      starting_index - 2 - 8
+    ]
+  end
 end
