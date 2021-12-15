32f26ccf19a1c0b52af46bff5e85219987191028
Zack Kayser <zkayser@cars.com>
2021-12-15 09:03:06 -0500

    Add basic tests for list of potential moves for a Knight given an empty board

--- test/chess/pieces/knight_test.exs
+++ test/chess/pieces/knight_test.exs
@@ -8,7 +8,7 @@ defmodule Chess.Pieces.KnightTest do
   alias Chess.Test.BoardHelpers
 
   describe "potential_moves/3 with empty board," do
-    test "allows knights in the middle of the board 4 potential moves" do
+    test "allows knights in the middle of the board 8 potential moves" do
       board = BoardHelpers.empty_board()
       starting_index = 35
       piece = %Piece{type: Knight}
@@ -18,6 +18,17 @@ defmodule Chess.Pieces.KnightTest do
 
       assert MapSet.new([18, 20, 25, 29, 41, 45, 50, 52]) ==
                Knight.potential_moves(piece, starting_index, board)
+    end
+
+    test "allows knights in the lower right corner 2 potential moves" do
+      board = BoardHelpers.empty_board()
+      starting_index = 63
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([46, 53]) == Knight.potential_moves(piece, starting_index, board)
     end
   end
 end
