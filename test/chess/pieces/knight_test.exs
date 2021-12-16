f8365bbe3218ea466b465cdd4fd875299cc749ca
Zack Kayser <zkayser@cars.com>
2021-12-16 17:38:54 -0500

    Add test cases for knights starting in the corners or on edges in the middle of the board

--- test/chess/pieces/knight_test.exs
+++ test/chess/pieces/knight_test.exs
@@ -30,5 +30,61 @@ defmodule Chess.Pieces.KnightTest do
 
       assert MapSet.new([46, 53]) == Knight.potential_moves(piece, starting_index, board)
     end
+
+    test "allows knights in the lower left corner 2 potential moves" do
+      board = BoardHelpers.empty_board()
+      starting_index = 56
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([41, 50]) == Knight.potential_moves(piece, starting_index, board)
+    end
+
+    test "allows knights in the upper left corner 2 potential moves" do
+      board = BoardHelpers.empty_board()
+      starting_index = 0
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([10, 17]) == Knight.potential_moves(piece, starting_index, board)
+    end
+
+    test "allows knights in the upper right corner 2 potential moves" do
+      board = BoardHelpers.empty_board()
+      starting_index = 7
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([13, 22]) == Knight.potential_moves(piece, starting_index, board)
+    end
+
+    test "allows knights in column 1 in the middle of the board 4 moves" do
+      board = BoardHelpers.empty_board()
+      starting_index = Board.coordinates_to_index({1, 4})
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([9, 18, 34, 41]) == Knight.potential_moves(piece, starting_index, board)
+    end
+
+    test "allows knights in column 8 in the middle of the board 4 moves" do
+      board = BoardHelpers.empty_board()
+      # Starting index => 31
+      starting_index = Board.coordinates_to_index({8, 4})
+      piece = %Piece{type: Knight}
+      square = :array.get(starting_index, board.board)
+      square = %Square{square | piece: piece}
+      board = %Board{board: :array.set(starting_index, square, board.board)}
+
+      assert MapSet.new([14, 21, 37, 46]) == Knight.potential_moves(piece, starting_index, board)
+    end
   end
 end
