8b49eeea01a5b8907f7294b04463b32f5bf0978f
Zack Kayser <zkayser@cars.com>
2021-12-20 07:47:37 -0500

    Add modueldoc to Chess.Pieces.MoveCache

--- lib/chess/pieces/move_cache.ex
+++ lib/chess/pieces/move_cache.ex
@@ -1,3 +1,8 @@
 defmodule Chess.Pieces.MoveCache do
+  @moduledoc """
+  A Nebulex-based cache for caching lists of potential moves per
+  piece given a starting index or coordinate
+  """
+
   use Nebulex.Cache, otp_app: :chess, adapter: Nebulex.Adapters.Local
 end
