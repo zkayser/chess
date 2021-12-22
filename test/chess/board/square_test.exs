51488888fe3877bf7879fbc77457160da47a3813
Zack Kayser <zkayser@cars.com>
2021-12-22 07:06:33 -0500

    Remove unused Square.init/1 function

--- test/chess/board/square_test.exs
+++ test/chess/board/square_test.exs
@@ -4,21 +4,6 @@ defmodule Chess.Board.SquareTest do
 
   alias Chess.Board.Square
   alias Chess.Piece
-  require Integer
-
-  describe "init/1" do
-    property "returns a white square when given index is even" do
-      check all(index <- filter(integer(0..63), &Integer.is_even/1)) do
-        assert %Square{color: :white} = Square.init(index)
-      end
-    end
-
-    property "returns a black square when given index is odd" do
-      check all(index <- filter(integer(0..63), &Integer.is_odd/1)) do
-        assert %Square{color: :black} = Square.init(index)
-      end
-    end
-  end
 
   describe "occupiable/2" do
     test "returns false if the square given is occupied by a piece of the same color" do
