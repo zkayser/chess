3d80c74dd0345ff2c8b3dd2a73869d32c4127fec
Zack Kayser <zkayser@cars.com>
2021-12-22 07:06:33 -0500

    Remove unused Square.init/1 function

--- lib/chess/board/square.ex
+++ lib/chess/board/square.ex
@@ -2,10 +2,6 @@ defmodule Chess.Board.Square do
   @moduledoc """
   Representation of a square on a chess board.
   """
-  use Oath
-
-  import Integer, only: [is_even: 1]
-
   alias Chess.Piece
 
   @type t() :: %__MODULE__{
@@ -18,15 +14,6 @@ defmodule Chess.Board.Square do
   defstruct color: nil,
             piece: nil
 
-  @decorate pre("Index is a non-negative integer between 0 and 64", fn index -> index in 0..64 end)
-  @spec init(non_neg_integer()) :: t()
-  def init(index) do
-    %__MODULE__{
-      color: color_for(index),
-      piece: Piece.for_starting_position(index)
-    }
-  end
-
   @spec occupiable?(t(), Piece.t()) :: boolean()
   def occupiable?(%__MODULE__{piece: %Piece{color: color}}, %Piece{color: color}), do: false
 
@@ -38,8 +25,4 @@ defmodule Chess.Board.Square do
   @spec occupied?(t()) :: boolean()
   def occupied?(%__MODULE__{piece: %Piece{}}), do: true
   def occupied?(_), do: false
-
-  @spec color_for(index()) :: color()
-  defp color_for(index) when is_even(index), do: :white
-  defp color_for(_index), do: :black
 end
