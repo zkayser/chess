3e9b567f3c7162e88a6bc9b5888f943a220e22c4
Zack Kayser <zkayser@cars.com>
2022-01-04 07:07:00 -0500

    Update caching strategy on Rook.potential_moves/

--- lib/chess/pieces/rook.ex
+++ lib/chess/pieces/rook.ex
@@ -2,6 +2,8 @@ defmodule Chess.Pieces.Rook do
   @moduledoc """
   Represents a Rook piece.
   """
+  use Nebulex.Caching
+
   @behaviour Chess.Piece
 
   alias Chess.Board
@@ -10,13 +12,16 @@ defmodule Chess.Pieces.Rook do
   @impl Piece
   def potential_moves(piece, starting_index, board) do
     starting_index
-    |> list_of_potential_moves(piece, board)
+    |> list_of_potential_moves()
+    |> filter_unreachable_coordinates(piece, board)
     |> Stream.map(&Board.coordinates_to_index/1)
     |> MapSet.new()
   end
 
-  @spec list_of_potential_moves(Board.index(), Piece.t(), Board.t()) :: Enumerable.t()
-  defp list_of_potential_moves(starting_index, piece, board) do
+  @decorate cacheable(cache: Chess.Pieces.MoveCache, key: {__MODULE__, starting_index})
+  @spec list_of_potential_moves(Board.index()) ::
+          list({list(Board.coordinates()), list(Board.coordinates())})
+  defp list_of_potential_moves(starting_index) do
     {starting_col, starting_row} = Board.index_to_coordinates(starting_index)
 
     {lateral, vertical} = {
@@ -32,19 +37,23 @@ defmodule Chess.Pieces.Rook do
     left = Enum.sort_by(left, &elem(&1, 0), :desc)
     below = Enum.sort_by(below, &elem(&1, 1), :desc)
 
-    lateral_moves =
-      Enum.concat(
-        reduce_until_blocked_or_capture(left, piece, board),
-        reduce_until_blocked_or_capture(right, piece, board)
-      )
+    [{left, right}, {below, above}]
+  end
 
-    vertical_moves =
+  @spec filter_unreachable_coordinates(
+          list({list(Board.coordinates()), list(Board.coordinates())}),
+          Piece.t(),
+          Board.t()
+        ) :: Enumerable.t()
+  defp filter_unreachable_coordinates(moves, piece, board) do
+    moves
+    |> Stream.map(fn {lower, higher} ->
       Enum.concat(
-        reduce_until_blocked_or_capture(below, piece, board),
-        reduce_until_blocked_or_capture(above, piece, board)
+        reduce_until_blocked_or_capture(lower, piece, board),
+        reduce_until_blocked_or_capture(higher, piece, board)
       )
-
-    Stream.concat(lateral_moves, vertical_moves)
+    end)
+    |> Stream.concat()
   end
 
   @spec reduce_until_blocked_or_capture(list(Board.coordinates()), Piece.t(), Board.t()) ::
