4bd29e0244377a7e5d5d3f314c4dd27857ee7583
Zack Kayser <zkayser@cars.com>
2022-04-13 10:51:28 -0400

    Add more comprehensive assertions to Perpendiculars.generate/1 unit test

--- test/chess/moves/generators/perpendiculars_test.exs
+++ test/chess/moves/generators/perpendiculars_test.exs
@@ -8,7 +8,13 @@ defmodule Chess.Moves.Generators.PerpendicularsTest do
     test "given starting index at {4, 4}, returns a list of sets along all perpendicals" do
       starting_index = Board.coordinates_to_index({4, 4})
 
-      assert [_laterals, _verticals] = Perpendiculars.generate(starting_index)
+      assert [{left, right} = _laterals, {below, above} = _verticals] =
+               Perpendiculars.generate(starting_index)
+
+      assert [{3, 4}, {2, 4}, {1, 4}] == left
+      assert [{5, 4}, {6, 4}, {7, 4}, {8, 4}] == right
+      assert [{4, 3}, {4, 2}, {4, 1}] == below
+      assert [{4, 5}, {4, 6}, {4, 7}, {4, 8}] == above
     end
   end
 end
