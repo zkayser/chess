2729e61527c6fefd6e3bc8c1344e468b26d808df
Zack Kayser <zkayser@cars.com>
2022-04-13 06:43:34 -0400

    Add comprehensive assertions to quadrants returned from Diagonals.generate/1 when starting at position 4, 4

--- test/chess/moves/generators/diagonals_test.exs
+++ test/chess/moves/generators/diagonals_test.exs
@@ -8,8 +8,12 @@ defmodule Chess.Moves.Generators.DiagonalsTest do
     test "given starting index at {4, 4}, returns a list of sets along all diagonals" do
       starting_index = Board.coordinates_to_index({4, 4})
 
-      assert [_quadrant_1, _quadrant_2, _quadrant_3, _quadrant_4] =
-               Diagonals.generate(starting_index)
+      assert [quadrant_1, quadrant_2, quadrant_3, quadrant_4] = Diagonals.generate(starting_index)
+
+      assert MapSet.new([{3, 3}, {2, 2}, {1, 1}]) == quadrant_1
+      assert MapSet.new([{5, 3}, {6, 2}, {7, 1}]) == quadrant_2
+      assert MapSet.new([{3, 5}, {2, 6}, {1, 7}]) == quadrant_3
+      assert MapSet.new([{5, 5}, {6, 6}, {7, 7}, {8, 8}]) == quadrant_4
     end
   end
 end
