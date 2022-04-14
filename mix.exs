d0770955f2569aa610c0fca1c224d7d7f72dc7e4
Zack Kayser <zkayser@cars.com>
2022-04-14 14:30:41 -0400

    Update liveview and live dashboard

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Chess.MixProject do
       {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.2.0"},
-      {:phoenix_live_dashboard, "~> 0.6.2"},
+      {:phoenix_live_dashboard, "~> 0.6.5"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
       {:plug_cowboy, "~> 2.5"},
