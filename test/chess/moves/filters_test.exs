7b92796aed0c4a31c2b491a479d67849cf3d5245
Zack Kayser <zkayser@cars.com>
2022-04-15 07:08:25 -0400

    Add another unit test on Filters.unreachable_coordinates/3 to test for moves up to and including an opposing piece's position

--- test/chess/moves/filters_test.exs
+++ test/chess/moves/filters_test.exs
@@ -27,8 +27,31 @@ defmodule Chess.Moves.FiltersTest do
 
       expected_filtered_moves = [{1, 2}, {1, 3}]
 
-      assert expected_filtered_moves ==
-               Filters.unreachable_coordinates([potential_moves_set], piece, board)
+      assert MapSet.new(expected_filtered_moves) ==
+               MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
+    end
+
+    test "reduces a set of moves in one direction up to and including an opposing piece but not beyond" do
+      board = BoardHelpers.empty_board()
+      piece = %Piece{type: Rook, color: :white}
+      friendly_piece = %Piece{type: Rook, color: :black}
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 1}), piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 4}), friendly_piece, board.grid)
+      }
+
+      potential_moves_set = for x <- 2..8, do: {1, x}
+
+      expected_filtered_moves = [{1, 2}, {1, 3}, {1, 4}]
+
+      assert MapSet.new(expected_filtered_moves) ==
+               MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
     end
   end
 end
