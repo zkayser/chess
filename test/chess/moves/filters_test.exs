5a7e88737f590191510e098b06c8af052884702b
Zack Kayser <zkayser@cars.com>
2022-04-15 07:55:03 -0400

    Add another test case to Filters.unreachable_coordinates checking for limited movements in every direction

--- test/chess/moves/filters_test.exs
+++ test/chess/moves/filters_test.exs
@@ -3,6 +3,7 @@ defmodule Chess.Moves.FiltersTest do
 
   alias Chess.Board
   alias Chess.Moves.Filters
+  alias Chess.Moves.Generators.{Diagonals, Perpendiculars}
   alias Chess.Piece
   alias Chess.Pieces.Rook
   alias Chess.Test.BoardHelpers
@@ -103,24 +104,91 @@ defmodule Chess.Moves.FiltersTest do
     test "limits moves in all directions up to where a piece can move no further due to obstruction" do
       board = BoardHelpers.empty_board()
       piece = %Piece{type: Rook, color: :white}
-      friendly_piece = %Piece{type: Rook, color: :black}
+      opposing_piece = %Piece{type: Rook, color: :black}
+
+      starting_coords = {4, 4}
+      starting_index = Board.coordinates_to_index(starting_coords)
 
       board = %Board{
         board
-        | grid: :array.set(Board.coordinates_to_index({1, 1}), piece, board.grid)
+        | grid: :array.set(starting_index, piece, board.grid)
       }
 
       board = %Board{
         board
-        | grid: :array.set(Board.coordinates_to_index({1, 4}), friendly_piece, board.grid)
+        | grid: :array.set(Board.coordinates_to_index({2, 4}), opposing_piece, board.grid)
       }
 
-      potential_moves_set = MapSet.new(for x <- 2..8, do: {1, x})
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({6, 4}), opposing_piece, board.grid)
+      }
 
-      expected_filtered_moves = [{1, 2}, {1, 3}, {1, 4}]
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({4, 2}), opposing_piece, board.grid)
+      }
 
-      assert MapSet.new(expected_filtered_moves) ==
-               MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({4, 6}), opposing_piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({2, 2}), opposing_piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({6, 6}), opposing_piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({6, 2}), opposing_piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({2, 6}), opposing_piece, board.grid)
+      }
+
+      potential_diagonal_moves = Diagonals.generate(starting_index)
+
+      potential_perpendicular_moves = Perpendiculars.generate(starting_index)
+
+      expected_left = [{3, 4}, {2, 4}]
+      expected_right = [{5, 4}, {6, 4}]
+      expected_below = [{4, 3}, {4, 2}]
+      expected_above = [{4, 5}, {4, 6}]
+      expected_quadrant_1 = [{3, 3}, {2, 2}]
+      expected_quadrant_2 = [{5, 5}, {6, 6}]
+      expected_quadrant_3 = [{5, 3}, {6, 2}]
+      expected_quadrant_4 = [{3, 5}, {2, 6}]
+
+      filtered_list =
+        MapSet.new(
+          Filters.unreachable_coordinates(
+            Stream.concat(potential_diagonal_moves, potential_perpendicular_moves),
+            piece,
+            board
+          )
+        )
+
+      expected =
+        List.flatten([
+          expected_left,
+          expected_right,
+          expected_below,
+          expected_above,
+          expected_quadrant_1,
+          expected_quadrant_2,
+          expected_quadrant_3,
+          expected_quadrant_4
+        ])
+
+      assert MapSet.new(expected) == filtered_list
     end
   end
 end
