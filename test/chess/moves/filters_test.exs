1a9f11e5e293abceed5df53f7745c766e445bc28
Zack Kayser <zkayser@cars.com>
2022-04-15 07:11:22 -0400

    Add more basic unit tests to Filters.unreachable_coordinates/3 given a list of mapsets as input

--- test/chess/moves/filters_test.exs
+++ test/chess/moves/filters_test.exs
@@ -53,5 +53,51 @@ defmodule Chess.Moves.FiltersTest do
       assert MapSet.new(expected_filtered_moves) ==
                MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
     end
+
+    test "takes a list of MapSets and filters moves up to a friendly piece's position" do
+      board = BoardHelpers.empty_board()
+      piece = %Piece{type: Rook, color: :white}
+      friendly_piece = %Piece{type: Rook, color: :white}
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 1}), piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 4}), friendly_piece, board.grid)
+      }
+
+      potential_moves_set = MapSet.new(for x <- 2..8, do: {1, x})
+
+      expected_filtered_moves = [{1, 2}, {1, 3}]
+
+      assert MapSet.new(expected_filtered_moves) ==
+               MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
+    end
+
+    test "takes a list of MapSets and filters moves up to and including an opposing piece's position" do
+      board = BoardHelpers.empty_board()
+      piece = %Piece{type: Rook, color: :white}
+      friendly_piece = %Piece{type: Rook, color: :black}
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 1}), piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 4}), friendly_piece, board.grid)
+      }
+
+      potential_moves_set = MapSet.new(for x <- 2..8, do: {1, x})
+
+      expected_filtered_moves = [{1, 2}, {1, 3}, {1, 4}]
+
+      assert MapSet.new(expected_filtered_moves) ==
+               MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
+    end
   end
 end
