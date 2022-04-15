796609616185dc915e78a402c83ff59b2bdf056c
Zack Kayser <zkayser@cars.com>
2022-04-15 08:02:45 -0400

    Updates Chess.Pieces.Rook to use Filters.unreachable_coordinates/3 instead of private filtering functions

--- lib/chess/pieces/rook.ex
+++ lib/chess/pieces/rook.ex
@@ -7,6 +7,7 @@ defmodule Chess.Pieces.Rook do
   @behaviour Chess.Piece
 
   alias Chess.Board
+  alias Chess.Moves.Filters
   alias Chess.Moves.Generators.Perpendiculars
   alias Chess.Piece
 
@@ -14,29 +15,8 @@ defmodule Chess.Pieces.Rook do
   def potential_moves(piece, starting_index, board) do
     starting_index
     |> Perpendiculars.generate()
-    |> filter_unreachable_coordinates(piece, board)
+    |> Filters.unreachable_coordinates(piece, board)
     |> Stream.map(&Board.coordinates_to_index/1)
     |> MapSet.new()
-  end
-
-  @spec filter_unreachable_coordinates(
-          Perpendiculars.t(),
-          Piece.t(),
-          Board.t()
-        ) :: Enumerable.t()
-  defp filter_unreachable_coordinates(moves, piece, board) do
-    Stream.flat_map(moves, &reduce_until_blocked_or_capture(&1, piece, board))
-  end
-
-  @spec reduce_until_blocked_or_capture(list(Board.coordinates()), Piece.t(), Board.t()) ::
-          list(Board.coordinates())
-  defp reduce_until_blocked_or_capture(potential_moves, %Piece{color: color}, board) do
-    Enum.reduce_while(potential_moves, [], fn coords, moves ->
-      case board[coords] do
-        %Piece{color: target_color} when target_color != color -> {:halt, [coords | moves]}
-        %Piece{color: target_color} when target_color == color -> {:halt, moves}
-        nil -> {:cont, [coords | moves]}
-      end
-    end)
   end
 end
