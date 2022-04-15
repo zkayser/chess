18e2ea995321ea65d28946e1269acae44edd9198
Zack Kayser <zkayser@cars.com>
2022-04-15 07:13:17 -0400

    Update variable name to reflect opposing piece is actually an opposing piece

--- test/chess/moves/filters_test.exs
+++ test/chess/moves/filters_test.exs
@@ -34,7 +34,7 @@ defmodule Chess.Moves.FiltersTest do
     test "reduces a set of moves in one direction up to and including an opposing piece but not beyond" do
       board = BoardHelpers.empty_board()
       piece = %Piece{type: Rook, color: :white}
-      friendly_piece = %Piece{type: Rook, color: :black}
+      opposing_piece = %Piece{type: Rook, color: :black}
 
       board = %Board{
         board
@@ -43,7 +43,7 @@ defmodule Chess.Moves.FiltersTest do
 
       board = %Board{
         board
-        | grid: :array.set(Board.coordinates_to_index({1, 4}), friendly_piece, board.grid)
+        | grid: :array.set(Board.coordinates_to_index({1, 4}), opposing_piece, board.grid)
       }
 
       potential_moves_set = for x <- 2..8, do: {1, x}
@@ -78,6 +78,29 @@ defmodule Chess.Moves.FiltersTest do
     end
 
     test "takes a list of MapSets and filters moves up to and including an opposing piece's position" do
+      board = BoardHelpers.empty_board()
+      piece = %Piece{type: Rook, color: :white}
+      opposing_piece = %Piece{type: Rook, color: :black}
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 1}), piece, board.grid)
+      }
+
+      board = %Board{
+        board
+        | grid: :array.set(Board.coordinates_to_index({1, 4}), opposing_piece, board.grid)
+      }
+
+      potential_moves_set = MapSet.new(for x <- 2..8, do: {1, x})
+
+      expected_filtered_moves = [{1, 2}, {1, 3}, {1, 4}]
+
+      assert MapSet.new(expected_filtered_moves) ==
+               MapSet.new(Filters.unreachable_coordinates([potential_moves_set], piece, board))
+    end
+
+    test "limits moves in all directions up to where a piece can move no further due to obstruction" do
       board = BoardHelpers.empty_board()
       piece = %Piece{type: Rook, color: :white}
       friendly_piece = %Piece{type: Rook, color: :black}
