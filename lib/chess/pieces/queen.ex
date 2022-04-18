deddf4c52af2f15b217b7164acbc612f1e43ae12
Zack Kayser <zkayser@cars.com>
2022-04-18 09:48:06 -0400

    Use flat_map/2 instead of map/2 + List.flatten/1

--- lib/chess/pieces/queen.ex
+++ lib/chess/pieces/queen.ex
@@ -16,8 +16,7 @@ defmodule Chess.Pieces.Queen do
       fetch_task(generator, starting_index, queen, board)
     end)
     |> Task.await_many()
-    |> Enum.map(&Enum.to_list/1)
-    |> List.flatten()
+    |> Enum.flat_map(&Enum.to_list/1)
   end
 
   @spec fetch_task(Diagonals | Perpendiculars, Board.index(), Piece.t(), Board.t()) :: Task.t()
