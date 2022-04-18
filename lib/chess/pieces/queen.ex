727b62ea54c58c9469aca60d640ba5ab31234128
Zack Kayser <zkayser@cars.com>
2022-04-18 09:34:20 -0400

    Implement Queen.potential_moves/3 callback

--- lib/chess/pieces/queen.ex
+++ lib/chess/pieces/queen.ex
@@ -2,4 +2,30 @@ defmodule Chess.Pieces.Queen do
   @moduledoc """
   Represents a Queen piece
   """
+  alias Chess.Board
+  alias Chess.Moves.Filters
+  alias Chess.Moves.Generators.{Diagonals, Perpendiculars}
+  alias Chess.Piece
+
+  @behaviour Piece
+
+  @impl Piece
+  def potential_moves(queen, starting_index, board) do
+    [Diagonals, Perpendiculars]
+    |> Enum.map(fn generator ->
+      fetch_task(generator, starting_index, queen, board)
+    end)
+    |> Task.await_many()
+    |> Enum.map(&Enum.to_list/1)
+    |> List.flatten()
+  end
+
+  @spec fetch_task(Diagonals | Perpendiculars, Board.index(), Piece.t(), Board.t()) :: Task.t()
+  defp fetch_task(generator, starting_index, queen, board) do
+    Task.async(fn ->
+      starting_index
+      |> generator.generate()
+      |> Filters.unreachable_coordinates(queen, board)
+    end)
+  end
 end
