de5c3a720d2a7ebea0f33222f25d629813421df7
Zack Kayser <zkayser@cars.com>
2022-10-03 08:53:05 -0400

    Create Chess.Game module

---
+++ lib/chess/game.ex
@@ -0,0 +1,15 @@
+defmodule Chess.Game do
+  @moduledoc """
+  A struct representing the state of a chess game
+  and functions for operating on the game instance.
+  """
+  alias Chess.Board
+
+  defstruct board: Board.layout(),
+            active_player: :white
+
+  @type t() :: %__MODULE__{
+          board: Board.t(),
+          active_player: :white | :black
+        }
+end
