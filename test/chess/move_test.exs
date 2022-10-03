102a98bc3fdf7db6ccc141ff93ccf8fb5e2c5e21
Zack Kayser <zkayser@cars.com>
2022-10-03 10:05:01 -0400

    Add several other property-based tests to move validation

--- test/chess/move_test.exs
+++ test/chess/move_test.exs
@@ -19,5 +19,29 @@ defmodule Chess.MoveTest do
                "Expected move with player #{player} from #{from} to #{to} to be valid, but it was not"
       end
     end
+
+    test "is invalid when player is neither black nor white" do
+      refute Move.changeset(%{player: "invalid", from: 0, to: 1}).valid?,
+             "Expected changeset to be invalid when player is not either `white` or `black`, but it was marked as valid"
+    end
+
+    property "is invalid when from is outside of the range 0..63" do
+      check(all(from <- one_of([integer(-100..-1), integer(64..100)]))) do
+        refute Move.changeset(%{player: "white", from: from, to: 1}).valid?,
+               "Expected changeset to be invalid when from is outside the range of 0..63, but it was marked valid with from = #{from}"
+      end
+    end
+
+    property "is invalid when to is outside of the range 0..63" do
+      check(all(to <- one_of([integer(-100..-1), integer(64..100)]))) do
+        refute Move.changeset(%{player: "white", from: 1, to: to}).valid?,
+               "Expected changeset to be invalid when to is outside the range of 0..63, but it was marked valid with to = #{to}"
+      end
+    end
+
+    test "is invalid when to and from are equal" do
+      refute Move.changeset(%{player: "white", from: 2, to: 2}).valid?,
+             "Expected changeset to be invalid when to is equal to from, but it was marked valid with both values equal"
+    end
   end
 end
