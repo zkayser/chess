4b587285efb2674e690c3978062c1b31d182a322
Zack Kayser <zkayser@cars.com>
2022-10-04 08:20:36 -0400

    First pass implementation at Game.play/2 function

--- lib/chess/game.ex
+++ lib/chess/game.ex
@@ -4,6 +4,8 @@ defmodule Chess.Game do
   and functions for operating on the game instance.
   """
   alias Chess.Board
+  alias Chess.Move
+  alias Chess.Piece
 
   defstruct board: Board.layout(),
             active_player: :white
@@ -19,5 +21,35 @@ defmodule Chess.Game do
   @spec new() :: t()
   def new do
     %__MODULE__{}
+  end
+
+  @spec play(t(), Move.attributes()) ::
+          {:ok, t()} | {:error, Ecto.Changeset.t(Move.t()) | String.t()}
+  def play(%__MODULE__{board: board} = game, move) do
+    with %{valid?: true} = changeset <- Move.changeset(move),
+         %Move{} = move <- Ecto.Changeset.apply_changes(changeset),
+         :player_matches <-
+           if(move.player == game.active_player, do: :player_matches, else: :player_mismatch),
+         true <- Enum.member?(Piece.potential_moves(board[move.from], move.from, board), move.to) do
+      grid = :array.set(move.to, board[move.from], board.grid)
+      grid = :array.set(move.from, nil, grid)
+
+      {:ok,
+       %__MODULE__{
+         game
+         | board: %Board{game.board | grid: grid},
+           active_player: if(game.active_player == :white, do: :black, else: :white)
+       }}
+    else
+      %Ecto.Changeset{} = changeset ->
+        {:error, changeset}
+
+      :player_mismatch ->
+        {:error,
+         "Player mismatch: Active Player #{inspect(game.active_player)}\nMoving player: #{move.player}"}
+
+      _ ->
+        {:error, "Invalid move -- From: #{move.from} to #{move.to} for player #{move.player}"}
+    end
   end
 end
