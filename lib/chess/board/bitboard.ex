b52e334d9ece884c0783cfed9ef4757aba9d26a4
Zack Kayser <zkayser@cars.com>
2022-10-28 08:56:51 -0400

    Implement position initialization and add to_list/2 function

--- lib/chess/board/bitboard.ex
+++ lib/chess/board/bitboard.ex
@@ -8,15 +8,43 @@ defmodule Chess.Boards.BitBoard do
   concurrent access into an array of 64-bit integers.
   https://www.erlang.org/doc/man/atomics.html
   """
+  use Bitwise
 
   defstruct [:ref]
 
-  @type t() :: %__MODULE__{ref: :atomics.atomics_ref()}
+  @opaque bitboards() :: :atomics.atomics_ref()
+  @type position() :: non_neg_integer()
+  @type t() :: %__MODULE__{ref: bitboards()}
+  @type bitboard() :: :positions
 
+  @positions_index 1
+  @bitboards %{positions: @positions_index}
   @bitboard_count 1
 
   @spec new() :: t()
   def new do
-    %__MODULE__{ref: :atomics.new(@bitboard_count, signed: false)}
+    bitboards = :atomics.new(@bitboard_count, signed: false)
+
+    _ = initialize_positions(bitboards)
+
+    %__MODULE__{ref: bitboards}
+  end
+
+  @spec to_list(t(), bitboard()) :: list()
+  def to_list(bitboard, bitboard_type \\ :positions) do
+    bitboard.ref
+    |> :atomics.get(@bitboards[bitboard_type])
+    |> Integer.digits(2)
+    |> Enum.chunk_every(8)
+  end
+
+  @spec initialize_positions(bitboards()) :: list(:ok)
+  defp initialize_positions(bitboards) do
+    for shift <- Enum.concat(0..15, 48..63) do
+      1
+      |> bsl(shift)
+      |> bor(:atomics.get(bitboards, @positions_index))
+      |> then(fn bitboard -> :atomics.put(bitboards, @positions_index, bitboard) end)
+    end
   end
 end
