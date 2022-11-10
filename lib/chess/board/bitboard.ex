c568ef26683c0d7d54ed39d7b699fe3d3a5fa779
Zack Kayser <zkayser@cars.com>
2022-11-10 10:49:31 -0500

    Refactor the bitboard initialization

--- lib/chess/board/bitboard.ex
+++ lib/chess/board/bitboard.ex
@@ -8,43 +8,85 @@ defmodule Chess.Boards.BitBoard do
   concurrent access into an array of 64-bit integers.
   https://www.erlang.org/doc/man/atomics.html
   """
-  use Bitwise
-
   defstruct [:ref]
 
   @opaque bitboards() :: :atomics.atomics_ref()
   @type position() :: non_neg_integer()
   @type t() :: %__MODULE__{ref: bitboards()}
-  @type bitboard() :: :positions
+  @type bitboard() ::
+          :composite
+          | :white_pawns
+          | :white_rooks
+          | :white_knights
+          | :white_bishops
+          | :white_queens
+          | :white_king
+          | :black_pawns
+          | :black_rooks
+          | :black_knights
+          | :black_bishops
+          | :black_queens
+          | :black_king
 
-  @positions_index 1
-  @bitboards %{positions: @positions_index}
-  @bitboard_count 1
+  @initial_boards [
+    {:composite, 18_446_462_598_732_906_495},
+    {:black_bishops, 2_594_073_385_365_405_696},
+    {:black_king, 576_460_752_303_423_488},
+    {:black_knights, 4_755_801_206_503_243_776},
+    {:black_pawns, 71_776_119_061_217_280},
+    {:black_queens, 1_152_921_504_606_846_976},
+    {:black_rooks, 9_295_429_630_892_703_744},
+    {:white_bishops, 36},
+    {:white_king, 8},
+    {:white_knights, 66},
+    {:white_pawns, 65280},
+    {:white_queens, 16},
+    {:white_rooks, 129}
+  ]
+
+  @atomics_offset 1
+
+  @bitboards @initial_boards
+             |> Enum.with_index(@atomics_offset)
+             |> Enum.map(fn {{key, _}, index} -> {key, index} end)
 
   @spec new() :: t()
   def new do
-    bitboards = :atomics.new(@bitboard_count, signed: false)
+    bitboards = :atomics.new(length(@bitboards), signed: false)
 
-    _ = initialize_positions(bitboards)
+    for {bitboard_type, bitboard_index} <- @bitboards do
+      :atomics.put(bitboards, bitboard_index, @initial_boards[bitboard_type])
+    end
 
     %__MODULE__{ref: bitboards}
   end
 
+  @doc """
+  Returns an 8x8 grid representation of a given
+  bitboard. If the bitboard does not take up a full
+  64 bits, the representation is padded with 0s to
+  create a full 8x8 grid. This function is intended
+  mainly for inspecting the state of a bitboard in
+  a way that is human-readable at a glance.
+  """
   @spec to_list(t(), bitboard()) :: list()
-  def to_list(bitboard, bitboard_type \\ :positions) do
+  def to_list(bitboard, bitboard_type \\ :composite) do
     bitboard.ref
     |> :atomics.get(@bitboards[bitboard_type])
     |> Integer.digits(2)
+    |> then(&with_padding/1)
     |> Enum.chunk_every(8)
   end
 
-  @spec initialize_positions(bitboards()) :: list(:ok)
-  defp initialize_positions(bitboards) do
-    for shift <- Enum.concat(0..15, 48..63) do
-      1
-      |> bsl(shift)
-      |> bor(:atomics.get(bitboards, @positions_index))
-      |> then(fn bitboard -> :atomics.put(bitboards, @positions_index, bitboard) end)
+  @spec with_padding(list(0 | 1)) :: list(0 | 1)
+  defp with_padding(board) do
+    board_length = Enum.count(board)
+
+    zeros = fn _ -> 0 end
+
+    case board_length < 64 do
+      true -> Enum.concat(Enum.map(board_length..63, zeros), board)
+      false -> board
     end
   end
 end
