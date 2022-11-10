3ec8d35886d10a99d2178a931166e8bc1e3b80e7
Zack Kayser <zkayser@cars.com>
2022-11-10 10:49:10 -0500

    Add set of unit tests asserting against inital bitboard state for each color piece along with the composite bitboard

--- test/chess/board/bitboard_test.exs
+++ test/chess/board/bitboard_test.exs
@@ -27,5 +27,185 @@ defmodule Chess.Boards.BitBoardTest do
                [1, 1, 1, 1, 1, 1, 1, 1]
              ] == BitBoard.to_list(bitboard, :composite)
     end
+
+    test "returns an 8x8 representation of the bitboard for black pawns" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 1, 1],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :black_pawns)
+    end
+
+    test "returns an 8x8 representation of the bitboard for black knights" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 1, 0, 0, 0, 0, 1, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :black_knights)
+    end
+
+    test "returns an 8x8 representation of the bitboard for black rooks" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [1, 0, 0, 0, 0, 0, 0, 1],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :black_rooks)
+    end
+
+    test "returns an 8x8 representation of the bitboard for black bishops" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 1, 0, 0, 1, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :black_bishops)
+    end
+
+    test "returns an 8x8 representation of the bitboard for the black queen" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 1, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :black_queens)
+    end
+
+    test "returns an 8x8 representation of the bitboard for the black king" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 1, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :black_king)
+    end
+
+    test "returns an 8x8 representation of the bitboard for white pawns" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 1, 1],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :white_pawns)
+    end
+
+    test "returns an 8x8 representation of the bitboard for white knights" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 1, 0, 0, 0, 0, 1, 0]
+             ] == BitBoard.to_list(bitboard, :white_knights)
+    end
+
+    test "returns an 8x8 representation of the bitboard for white rooks" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 0, 0, 0, 0, 0, 0, 1]
+             ] == BitBoard.to_list(bitboard, :white_rooks)
+    end
+
+    test "returns an 8x8 representation of the bitboard for white bishops" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 1, 0, 0, 1, 0, 0]
+             ] == BitBoard.to_list(bitboard, :white_bishops)
+    end
+
+    test "returns an 8x8 representation of the bitboard for the white queen" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :white_queens)
+    end
+
+    test "returns an 8x8 representation of the bitboard for the white king" do
+      bitboard = BitBoard.new()
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 1, 0, 0, 0]
+             ] == BitBoard.to_list(bitboard, :white_king)
+    end
   end
 end
