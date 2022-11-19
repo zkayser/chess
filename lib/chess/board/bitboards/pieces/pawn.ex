aaca87526955036d119a10e43ef7c42fc1528aeb
Zack Kayser <zkayser@cars.com>
2022-11-19 11:27:55 -0500

    Add second simple unit test for simple case with black pawns and single pushes

---
+++ lib/chess/board/bitboards/pieces/pawn.ex
@@ -0,0 +1,23 @@
+defmodule Chess.BitBoards.Pieces.Pawn do
+  @moduledoc """
+  Functions for calculating pawn single and double pushes,
+  attacks, pseudo-legal moves, and legal moves.
+  """
+  use Bitwise
+
+  alias Chess.Boards.BitBoard
+  alias Chess.Color
+
+  @single_push 8
+  @double_push 16
+
+  @spec single_pushes(BitBoard.t(), Color.t()) :: Bitboard.bitboard()
+  def single_pushes(bitboard, color) do
+    bitboard
+    |> BitBoard.get(:"#{color}_pawns")
+    |> operation(color).(@single_push)
+  end
+
+  defp operation(:black), do: &Bitwise.>>>/2
+  defp operation(:white), do: &Bitwise.<<</2
+end
