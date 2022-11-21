781cf50d2a8795ff29e09a22146bc8f0d7c012e8
Zack Kayser <zkayser@cars.com>
2022-11-21 07:41:49 -0500

    Add unit test cases for base Pawn.double_pushes/2 function

--- test/chess/board/bitboards/pieces/pawn_test.exs
+++ test/chess/board/bitboards/pieces/pawn_test.exs
@@ -3,6 +3,7 @@ defmodule Chess.BitBoards.Pieces.PawnTest do
 
   alias Chess.Boards.BitBoard
   alias Chess.BitBoards.Pieces.Pawn
+  alias Chess.Color
 
   describe "single_pushes/2" do
     test "given an initial state Bitboard.t/0 and white color, pushes all pawns up one rank" do
@@ -30,6 +31,40 @@ defmodule Chess.BitBoards.Pieces.PawnTest do
                [0, 0, 0, 0, 0, 0, 0, 0],
                [1, 1, 1, 1, 1, 1, 1, 1],
                [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_grid(single_pushes)
+    end
+  end
+
+  describe "double_pushes/2" do
+    test "given an initial state Bitboard.t/0 and white color, pushes all pawns up two ranks" do
+      bitboard = BitBoard.new()
+      double_pushes = Pawn.double_pushes(bitboard, Color.white())
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 1, 1],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0]
+             ] == BitBoard.to_grid(single_pushes)
+    end
+
+    test "given an initial state Bitboard.t/0 and black color, pushes all pawns down two ranks" do
+      bitboard = BitBoard.new()
+      double_pushes = Pawn.double_pushes(bitboard, Color.black())
+
+      assert [
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 1, 1],
                [0, 0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 0],
