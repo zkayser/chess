61f550816745359b96e0198e8e5afafcbd3977c9
Zack Kayser <zkayser@cars.com>
2022-11-21 07:45:13 -0500

    Implement Pawn.double_pushes/2 function

--- lib/chess/board/bitboards/pieces/pawn.ex
+++ lib/chess/board/bitboards/pieces/pawn.ex
@@ -11,11 +11,18 @@ defmodule Chess.BitBoards.Pieces.Pawn do
   @single_push 8
   @double_push 16
 
-  @spec single_pushes(BitBoard.t(), Color.t()) :: Bitboard.bitboard()
+  @spec single_pushes(BitBoard.t(), Color.t()) :: BitBoard.bitboard()
   def single_pushes(bitboard, color) do
     bitboard
     |> BitBoard.get(:"#{color}_pawns")
     |> operation(color).(@single_push)
+  end
+
+  @spec double_pushes(BitBoard.t(), Color.t()) :: BitBoard.bitboard()
+  def double_pushes(bitboard, color) do
+    bitboard
+    |> BitBoard.get(:"#{color}_pawns")
+    |> operation(color).(@double_push)
   end
 
   defp operation(:black), do: &Bitwise.>>>/2
