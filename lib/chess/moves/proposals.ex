e1fc3675fa539f5926e8d8cf38c8fd4abe96c848
Zack Kayser <zkayser@gmail.com>
2023-05-04 15:53:10 -0400

    Declare types for inputs and input strings; return error when source and destination are equal

--- lib/chess/moves/proposals.ex
+++ lib/chess/moves/proposals.ex
@@ -12,7 +12,30 @@ defmodule Chess.Moves.Proposals do
             destination: nil
 
   @opaque coordinates() :: {file :: String.t(), rank :: integer()}
+
+  @typedoc """
+  An `input_string` is a 16-bit (2-byte) string
+  containing a rank and file, where each takes up
+  1 byte (8 bits each, 16 bits total)
+  """
+  @type input_string() :: <<_::16>>
+
+  @typedoc """
+  A map of inputs contains a source and destination input string,
+  representing the move being proposed.
+  """
+  @type inputs() :: %{
+          source: input_string(),
+          destination: input_string()
+        }
   @type t() :: %__MODULE__{source: coordinates(), destination: coordinates()}
+
+  @spec from_inputs(inputs() | any()) ::
+          {:ok, t()}
+          | {:error, {:invalid_inputs, term()}}
+          | {:error, :source_and_destination_equal}
+  def from_inputs(%{source: source, destination: destination}) when source == destination,
+    do: {:error, :source_and_destination_equal}
 
   def from_inputs(%{
         source: <<source_file::8, source_rank::8>>,
