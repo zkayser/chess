46efd66bf85f4de196ba96aa0e34de9148217062
Zack Kayser <zkayser@gmail.com>
2023-06-19 09:46:25 -0400

    Add unit test for Slider.bishop/0

--- test/chess/board/bitboards/slider_test.exs
+++ test/chess/board/bitboards/slider_test.exs
@@ -10,8 +10,14 @@ defmodule Chess.Bitboards.SliderTest do
   end
 
   describe "rook/0" do
-    test "returns the deltas for sliding a rook piece" do
+    test "returns the slider deltas for a rook sliding piece" do
       assert %Slider{deltas: [{1, 0}, {0, -1}, {-1, 0}, {0, 1}]} == Slider.rook()
+    end
+  end
+
+  describe "bishop/0" do
+    test "returns the slider deltas for a bishop sliding piece" do
+      assert %Slider{deltas: [{1, 1}, {1, -1}, {-1, -1}, {-1, 1}]} == Slider.bishop()
     end
   end
 end
