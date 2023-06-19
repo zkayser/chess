3a372f48ec8bae98a92773d9b6e9531850f18c50
Zack Kayser <zkayser@gmail.com>
2023-06-19 09:43:25 -0400

    Add unit test for Slider.rook/0 function

--- test/chess/board/bitboards/slider_test.exs
+++ test/chess/board/bitboards/slider_test.exs
@@ -3,9 +3,15 @@ defmodule Chess.Bitboards.SliderTest do
 
   alias Chess.Bitboards.Slider
 
-  describe "Slider" do
+  describe "Slider struct" do
     test "is a struct containing a list of deltas" do
       assert %Slider{deltas: [{0, 1}, {1, 0}, {0, -1}, {-1, 0}]}
+    end
+  end
+
+  describe "rook/0" do
+    test "returns the deltas for sliding a rook piece" do
+      assert %Slider{deltas: [{1, 0}, {0, -1}, {-1, 0}, {0, 1}]} == Slider.rook()
     end
   end
 end
