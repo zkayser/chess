c78d82b0e7e75710cc1c825d3f1cc684ae54af40
Zack Kayser <zkayser@gmail.com>
2026-06-08 10:11:49 -0400

    Refactor: Remove tests for chess piece movement and board helpers
    - Deleted tests for chess piece movement including Rook, Bishop, Knight, Pawn, Queen, and their respective generators.
    - Removed the BoardHelpers module used for generating empty boards in tests.
    - Cleaned up associated test files to streamline the codebase and focus on core functionality.

--- lib/chess/pieces/move_cache.ex
+++
@@ -1,8 +0,0 @@
-defmodule Chess.Pieces.MoveCache do
-  @moduledoc """
-  A Nebulex-based cache for caching lists of potential moves per
-  piece given a starting index or coordinate
-  """
-
-  use Nebulex.Cache, otp_app: :chess, adapter: Nebulex.Adapters.Local
-end
